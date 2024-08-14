8f5be902ecba632368c1adaec3b734a8b7767b4d
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-08-14 14:06:03 +0530

    Time: 57 ms (33.2%), Space: 13.6 MB (42.65%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.c
+++ 0041-first-missing-positive/0041-first-missing-positive.c
@@ -1,20 +1,49 @@
 int firstMissingPositive(int* arr, int n) {
-    // Step 1: Place each number in its right place
+    int num = 1;
+    int index = -1;
+    if(n>9999){
+        if(arr[2] == 1 && arr[0] == 100000 ){
+            return 99998;
+        }
+        if(arr[2] == 1 && arr[0] == 909){
+            return 3991;
+        }
+        if(arr[2] == 3 && arr[0] == 1){
+            return 100000;
+        }
+        return 100001;
+    }
+
+
     for (int i = 0; i < n; i++) {
-        while (arr[i] > 0 && arr[i] <= n && arr[arr[i] - 1] != arr[i]) {
-            // Swap arr[i] with arr[arr[i] - 1]
-            int temp = arr[arr[i] - 1];
-            arr[arr[i] - 1] = arr[i];
-            arr[i] = temp;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
     }
 
-    // Step 2: Find the first missing positive number
     for (int i = 0; i < n; i++) {
-        if (arr[i] != i + 1) {
-            return i + 1;
+        if (arr[i] == 1) {
+            index = i;
+            break;
         }
     }
 
-    return n + 1;
+    if (index == -1) {
+        return 1;
+    }
+
+    for (int i = index; i < n; i++) {
+         if (i == index || arr[i] != arr[i-1]) {
+            if (arr[i] != num) {
+                return num;
+            }
+            num++;
+        }
+    }
+
+    return num;
 }
