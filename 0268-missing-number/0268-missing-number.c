29bd5467d1dec976f2fd2a6ec70ead32686051cc
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-08-15 11:55:14 +0530

    Time: 12 ms (73.91%), Space: 8.9 MB (16.3%) - LeetHub

--- 0268-missing-number/0268-missing-number.c
+++ 0268-missing-number/0268-missing-number.c
@@ -1,16 +1,8 @@
+
 int missingNumber(int* arr, int n) {
-    int dup[n+1];
-    int result = 0;
-    for(int i=0;i<n+1;i++){
-        dup[i] = 0;
-    }
+    int sum = 0;
     for(int i=0;i<n;i++){
-        dup[arr[i]]++;
+     sum = sum + arr[i];
     }
-    for(int i=0;i<n+1;i++){
-        if(dup[i] == 0){
-            result = i;
-        }
-    }
-    return result;
+    return ((n*(n+1))/2)-sum;
 }
