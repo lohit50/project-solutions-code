d9a0226e44ae4c76fc121c42a6612c90742faab8
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-08-21 23:49:18 +0530

    Time: 0 ms (100%), Space: 8.4 MB (20.48%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.c
+++ 0029-divide-two-integers/0029-divide-two-integers.c
@@ -1,7 +1,7 @@
 int divide(int dividend, int divisor) {
-    // Handle the overflow case where the result exceeds the 32-bit signed integer range
+    
     if (dividend == INT_MIN && divisor == -1) {
-        return INT_MAX; // Return 2147483647 to avoid overflow
+        return INT_MAX;
     }
     return dividend / divisor;
 }
