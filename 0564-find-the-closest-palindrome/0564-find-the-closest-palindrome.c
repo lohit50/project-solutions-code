20010d04a7c1ac9f896aa04c7b7c4b06efebc26a
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-08-24 20:45:04 +0530

    Time: 0 ms (100%), Space: 7.7 MB (60%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.c
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.c
@@ -1,4 +1,4 @@
-long parseLong(char* n){
+long parseLong(char* n) {
     long r = 0;
     for(int i = 0; n[i]; i++)
         r = r * 10 + (n[i] - '0');
@@ -27,44 +27,41 @@ char* clone(char* n, int len) {
     return t;
 }
 
-char* nearestPalindromic(char* t) { // Let t = 10000
+char* nearestPalindromic(char* t) {
     int len = strlen(t);
     int tLen = len + 1;
-    int mid = tLen / 2 ;
-    
+    int mid = tLen / 2;
+
     if(len == 1) {
         --*t;
         return t;
     }
-    
-    char* t1 = clone(t, len);       // t1 = 0100??
-    char* t2 = clone(t, len);       // t2 = 0100??
-    char* t3 = clone(t, len);       // t3 = 0100??
-    
-    //Mid
-    int left = 1, right = tLen - 1;
-    for (int i = 0; i <= mid; i++)  
-        t1[right - i] = t1[left + i];//t1 = 010001
 
-    //Less
-    minusOne(t2 + mid);              //t2 = 0099??
-    for (int i = 0; i <= mid; i++) 
-        t2[right - i] = t2[left + i];//t2 = 009990
+    char* t1 = clone(t, len);
+    char* t2 = clone(t, len);
+    char* t3 = clone(t, len);
+
+    int left = 1, right = tLen - 1;
+    for (int i = 0; i <= mid; i++)
+        t1[right - i] = t1[left + i];
+
+    minusOne(t2 + mid);
+    for (int i = 0; i <= mid; i++)
+        t2[right - i] = t2[left + i];
 
     if (t2[left] == '0')
-        t2[right] = '9';             //t2 = 009999
+        t2[right] = '9';
 
-    //Greater
-    plusOne(t3 + mid);               //t3 = 0101??
-    if(*t3 != '0') left = 0;         //This part is for the cases: 09 + 1 = 10, 099 + 1 = 100, and so on.
+    plusOne(t3 + mid);
+    if(*t3 != '0') left = 0;
     for (int i = 0; i <= mid; i++)
-        t3[right - i] = t3[left + i];//t3 = 010101
-    
-    long ln  = parseLong(t); 
-    long ln1 = parseLong(t1) /*mid*/    , diff1 = abs(ln1 - ln); 
-    long ln2 = parseLong(t2) /*less*/   , diff2 = abs(ln2 - ln); 
-    long ln3 = parseLong(t3) /*greater*/, diff3 = abs(ln3 - ln); 
-    
+        t3[right - i] = t3[left + i];
+
+    long ln = parseLong(t);
+    long ln1 = parseLong(t1), diff1 = abs(ln1 - ln);
+    long ln2 = parseLong(t2), diff2 = abs(ln2 - ln);
+    long ln3 = parseLong(t3), diff3 = abs(ln3 - ln);
+
     char* res;
     if (ln > ln1)
         res = diff1 > diff3 ? t3 : t1;
@@ -72,7 +69,7 @@ char* nearestPalindromic(char* t) { // Let t = 10000
         res = diff2 > diff1 ? t1 : t2;
     else
         res = diff2 > diff3 ? t3 : t2;
-    
+
     while (*res == '0') res++;
 
     return res;
