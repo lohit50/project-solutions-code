e6a3dc2b22f07629b67fda9c3110d45bc4bfd6a3
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-08-25 20:59:21 +0530

    Time: 0 ms (100%), Space: 7.8 MB (66.09%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.c
+++ 0058-length-of-last-word/0058-length-of-last-word.c
@@ -1,17 +1,21 @@
-#include <ctype.h>
-#include <string.h>
 
 int lengthOfLastWord(char* arr) {
-    int n = strlen(arr); 
     int count = 0;
-    
+    int n = 0;
+
+    // Find the length of the string while traversing
+    while (arr[n] != '\0') {
+        n++;
+    }
+
+    // Traverse from the end to count the last word
     for (int i = n - 1; i >= 0; i--) {
         if (isalpha(arr[i])) {
             count++;
-        }
-        else if (count > 0) {
+        } else if (count > 0) {
             break;
         }
     }
+
     return count;
 }
