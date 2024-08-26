ef92f5938c9a02942a30de23cef14550642a453b
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-08-26 09:41:23 +0530

    Time: 48 ms (47.91%), Space: 10.4 MB (59.16%) - LeetHub

--- 0055-jump-game/0055-jump-game.c
+++ 0055-jump-game/0055-jump-game.c
@@ -2,11 +2,11 @@ bool canJump(int* arr, int n) {
     int maxReach = 0;
     for (int jumped = 0; jumped < n; jumped++) {
         if (jumped > maxReach) {
-            return false;  
+            return false;  // If jumped index is beyond maxReach, it's not possible to proceed.
         }
-        maxReach = jumped + arr[jumped];
+        maxReach = (jumped + arr[jumped] > maxReach) ? jumped + arr[jumped] : maxReach;
         if (maxReach >= n - 1) {
-            return true; 
+            return true;  // If maxReach reaches or exceeds the last index, return true.
         }
     }
     return false;
