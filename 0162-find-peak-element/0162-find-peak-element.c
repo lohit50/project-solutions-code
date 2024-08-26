ce1c1b5865ee59762d74e0b0d61e4ce50e3543d6
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-08-26 11:34:28 +0530

    Time: 5 ms (41.1%), Space: 8.2 MB (15.38%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.c
+++ 0162-find-peak-element/0162-find-peak-element.c
@@ -1,23 +1,17 @@
 int findPeakElement(int* arr, int n) {
-    int i=0;
-    if(n==3){
-        if(arr[0]>arr[1]) return 0;
-        else if(arr[2]>arr[1]) return 2;
-        else return 1;
+    if (n == 1) return 0;
+    if (n == 2) {
+        if (arr[0] > arr[1]) return 0;
+        return 1;
     }
-    if(n<=2){
-        if(n==1) return 0;
-        if(arr[0] > arr[1]) return 0;
-        else return 1;
-    }
-    for(i=0;i<n;i++){
-        if(arr[n-1]>arr[n-2]) return n-1;
-        if(arr[0]>arr[1]) return 0;
-        if(i>=1 && i<n-1){
-            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-                return i;
-            }
+
+    if (arr[0] > arr[1]) return 0;
+    if (arr[n-1] > arr[n-2]) return n-1;
+
+    for (int i = 1; i < n - 1; i++) {
+        if (arr[i] > arr[i-1] && arr[i] > arr[i+1]) {
+            return i;
         }
     }
-    return i;
+    return 69;
 }
