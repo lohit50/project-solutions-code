2d7619931253ce1ac151ebe8fe4f2ebf4cebdd85
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-08-26 15:17:44 +0530

    Time: 4 ms (98.49%), Space: 10.3 MB (18.09%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.c
+++ 0229-majority-element-ii/0229-majority-element-ii.c
@@ -1,40 +1,25 @@
 int* majorityElement(int* arr, int n, int* returnSize) {
-    int* result = (int*)malloc(2 * sizeof(int)); 
-    int count1 = 0, count2 = 0, candidate1 = 0, candidate2 = 0;
+    int* result = (int*)malloc(n * sizeof(int)); 
+    int resultIndex = 0;
+    int temp = 0;
 
     for (int i = 0; i < n; i++) {
-        if (candidate1 == arr[i]) {
-            count1++;
-        } else if (candidate2 == arr[i]) {
-            count2++;
-        } else if (count1 == 0) {
-            candidate1 = arr[i];
-            count1 = 1;
-        } else if (count2 == 0) {
-            candidate2 = arr[i];
-            count2 = 1;
-        } else {
-            count1--;
-            count2--;
+        int count = 0;
+        if(arr[i] == INT_MIN){
+            continue;
+        }
+        for (int j = 0; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                temp = arr[i];
+                if(count>1) arr[j] = INT_MIN;
+            }
+        }
+        if (count > n / 3) {
+           result[resultIndex++] = temp;
         }
     }
 
-    count1 = count2 = 0;
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == candidate1) {
-            count1++;
-        } else if (arr[i] == candidate2) {
-            count2++;
-        }
-    }
-
-    *returnSize = 0;
-    if (count1 > n / 3) {
-        result[(*returnSize)++] = candidate1;
-    }
-    if (count2 > n / 3) {
-        result[(*returnSize)++] = candidate2;
-    }
-
+    *returnSize = resultIndex;
     return result;
 }
