7398087601eaf296370d553fbbf771cf0d58a78a
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-08-29 21:53:50 +0530

    Time: 31 ms (13.98%), Space: 46.2 MB (49.96%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,14 +1,16 @@
 class Solution {
     public int[] nextGreaterElements(int[] arr1) {
         int n = arr1.length;
-        int[] result = new int[n];
+        int[] arr2 = new int[arr1.length * 2];
+        System.arraycopy(arr1, 0, arr2, 0, arr1.length);
+        System.arraycopy(arr1, 0, arr2, arr1.length, arr1.length);
         
+        int[] result = new int[arr1.length];
         for (int i = 0; i < n; i++) {
             int flag = 0;
             for (int j = i; j < i + n; j++) {
-                int index = j % n;
-                if (arr1[index] > arr1[i]){
-                    result[i] = arr1[index];
+                if (arr2[j] > arr1[i]) {
+                    result[i] = arr2[j];
                     flag = 1;
                     break;
                 }
