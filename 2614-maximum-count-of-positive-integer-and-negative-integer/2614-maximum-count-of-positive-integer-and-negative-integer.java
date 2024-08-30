90c8fd28686b616a6a33a61d3a644a814b3c1e05
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-08-30 21:12:54 +0530

    Time: 0 ms (100%), Space: 44.8 MB (49.43%) - LeetHub

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        int n = nums.length;
+        int countNegative = 0;
+        int countPositive = 0;
+
+        if(nums[0] < 0 && nums[n-1] < 0){
+            return n;
+        }else if(nums[0] > 0 && nums[n-1] > 0){
+            return n;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
+                countNegative++;
+            } else if (nums[i] > 0) {
+                countPositive++;
+            }
+        }
+
+        return Math.max(countNegative, countPositive);
+    }
+}
