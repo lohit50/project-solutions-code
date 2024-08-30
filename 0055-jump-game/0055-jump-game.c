edd1632a081f47d324faad235468dc182d75f206
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-08-30 11:52:58 +0530

    Time: 49 ms (37.95%), Space: 10.6 MB (21.5%) - LeetHub

--- 0055-jump-game/0055-jump-game.c
+++ 0055-jump-game/0055-jump-game.c
@@ -1,12 +1,16 @@
+#include <stdbool.h>
 
 bool canJump(int* nums, int numsSize) {
+    int maxReach = 0;
+
     for (int i = 0; i < numsSize; i++) {
-        if (i > nums[0]) {
-            return false;  
+        if (i > maxReach) {
+            return false;
         }
-        if (i + nums[i] > nums[0]) {
-            nums[0] = i + nums[i]; 
+        if (i + nums[i] > maxReach) {
+            maxReach = i + nums[i];
         }
     }
+
     return true;
 }
