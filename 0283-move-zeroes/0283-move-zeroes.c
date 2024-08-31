4a50c8187865e106a853a1983d57431f2575046a
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-08-31 23:20:26 +0530

    Time: 70 ms (82.96%), Space: 19.8 MB (15.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,14 +1,10 @@
 void moveZeroes(int* nums, int n) {
-    int count = 1;
+    int count = 0;
     for(int i=0;i<n;i++){
-        if(nums[i] == 0){
-           for(int j=n-1;j>i;j--){
-            if(nums[j] != 0){
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-            }
-          }
-       }
+        if(nums[i] != 0){
+            int temp = nums[count];
+            nums[count++] = nums[i];
+            nums[i] = temp; 
+        }
     }
 }
