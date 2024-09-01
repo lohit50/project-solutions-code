25fb4faf7689013f5704c346bfa8db7122ae0596
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-09-01 22:47:00 +0530

    Time: 0 ms (100%), Space: 9.6 MB (33.39%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.c
+++ 0075-sort-colors/0075-sort-colors.c
@@ -1,28 +1,12 @@
 void sortColors(int* nums, int n) {
-    int zero = 0;
-    int two = 0;
-    int one = 0;
+    int count[3] = {0};
     for(int i=0;i<n;i++){
-        if(nums[i] == 0){
-            zero++;
-        }
-        if(nums[i] == 1){
-            one++;
-        }
-        if(nums[i] == 2){
-            two++;
-        }
+        count[nums[i]]++;
     }
-    for(int i=0;i<n;i++){
-        if(zero){
-            nums[i] = 0;
-            zero--;
-        }else if(one){
-            nums[i] = 1;
-            one--;
-        }else if(two){
-            nums[i] = 2;
-            two--;
+    int j=0;
+    for(int i=0;i<3;i++){
+        while(count[i]--){
+            nums[j++] = i;
         }
     }
 }
