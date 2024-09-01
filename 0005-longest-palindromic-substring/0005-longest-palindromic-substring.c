3fb7539d8c92f4d734edcb27f92d963fd182441b
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-09-01 09:50:29 +0530

    Time: 8 ms (89.82%), Space: 8.3 MB (37.34%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.c
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.c
@@ -1,24 +1,22 @@
 
 char* longestPalindrome(char* s) {
-    int n = strlen(s);
-    int start = 0;
-    int maxlen = 1;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 2; j++) {
+   int n = strlen(s);
+   int maxlen = 1;
+   int start = 0;
+   for(int i=0;i<n;i++){
+        for(int j=0;j<2;j++){
             int left = i;
-            int right = i + j;
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                if (right - left + 1 > maxlen) {
-                    maxlen = right - left + 1;
-                    start = left;
+            int right = i+j;
+            while(left>=0 && right<n && s[left] == s[right]){
+                if(right-left+1 > maxlen){
+                maxlen = right-left+1;
+                start = left;
                 }
-                left--;
-                right++;
+             left--;
+             right++;
             }
         }
     }
-
-    s[start + maxlen] = '\0';
-    return s + start;
+    s[start+maxlen] = '\0';
+    return s+start;
 }
