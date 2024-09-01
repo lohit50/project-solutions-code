286aa3b6d81ed2726f53aea2a7f1afa05f99815c
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-09-01 10:10:51 +0530

    Time: 240 ms (5.26%), Space: 9.9 MB (13.77%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.c
@@ -0,0 +1,29 @@
+int trap(int* height, int n) {
+    int traped = 0;
+    for (int i = n - 1; i > 0; i--) { 
+        int greatest = -1;
+        int last_index = 0;
+
+        for (int j = i - 1; j >= 0; j--) {
+            if (height[j] > greatest) {
+                greatest = height[j];
+                last_index = j;
+            }
+            if (height[j] >= height[i]) {
+                last_index = j;
+                greatest = height[i];
+                break;
+            }
+        }
+        
+        if (greatest > 0) {
+            for (int k = last_index + 1; k < i; k++) {
+                traped += greatest - height[k];
+            }
+        }
+        
+        i = last_index+1;
+    }
+    
+    return traped;
+}
