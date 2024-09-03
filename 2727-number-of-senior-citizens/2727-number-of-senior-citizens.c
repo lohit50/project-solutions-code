ea9fb0772ff8f738b081f6b446b2e22a36145a10
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-09-03 17:27:52 +0530

    Time: 7 ms (35.86%), Space: 8.7 MB (77.61%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.c
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.c
@@ -1,7 +1,7 @@
 int countSeniors(char ** details, int n){
     int count = 0;
     for(int i=0;i<n;i++){
-        if(((details[i][11]-'0')*10 + (details[i][12]-'0')) > 60){
+        if(details[i][11] > '6' || details[i][11]=='6' && details[i][12]>'0'){
             count++;
         }
     }
