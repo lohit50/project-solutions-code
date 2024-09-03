f2db94798e6c113bf7d0ea8aa9338b6951ff5f2c
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-09-03 09:25:02 +0530

    Time: 0 ms (100%), Space: 8 MB (39.13%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.c
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.c
@@ -1,30 +1,22 @@
 int getLucky(char* s, int k) {
     int sum = 0;
-    int total = 0;
     int n = strlen(s);
     for(int i=0;i<n;i++){
-        if((s[i]-'`')>9){
-            int num =(s[i]-'`');
+            int num =(s[i]-'a'+1);
             while(num>0){
                 sum+=num%10;
                 num/=10;
             }
-        }
-        else{
-            sum+=(s[i]-'`');
-        }
     }
-    if(k==1){
-        return sum;
-    }else{
-        for(int i=1;i<k;i++){
-            total = 0;
-            while(sum>0){
-                total += sum%10;
-                sum/=10;
-            }
-            sum = total;
+
+    while(--k>0){
+        int total = 0;
+         while(sum>0){
+            total += sum%10;
+            sum/=10;
         }
+         sum = total;
     }
-    return total;
+    
+    return sum;
 }
