c4890fcee17e55459156e6eb8456abb0bc8ff366
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-09-04 20:20:15 +0530

    Time: 7 ms (28.22%), Space: 9.3 MB (6.22%) - LeetHub

--- 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.c
+++ 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.c
@@ -1,6 +1,7 @@
-int addedInteger(int* nums1, int n1, int* nums2, int n2) {
-    int sum = 0;
-    for(int i=0;i<n1;i++) sum+= nums2[i]-nums1[i];
-    
-    return sum/n2;
+int addedInteger(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int sumDiff = 0;
+    for(int i=0;i<nums1Size;i++){
+        sumDiff+= nums2[i]-nums1[i];
+    }
+    return sumDiff/nums1Size;
 }
