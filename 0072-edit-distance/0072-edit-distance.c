631117529316fabca06dcd53e522fec566635b2a
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-09-04 09:30:11 +0530

    Time: 7 ms (53.04%), Space: 9.5 MB (70.87%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.c
+++ 0072-edit-distance/0072-edit-distance.c
@@ -5,22 +5,20 @@ int minDistance(char* word1, char* word2) {
     int dp[m+1][n+1];
     
     for (int i = 0; i <= m; i++) {
-        dp[i][0] = i; 
-    }
-    for (int j = 0; j <= n; j++) {
-        dp[0][j] = j;
-    }
-    
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            int cost = (word1[i - 1] == word2[j - 1]) ? 0 : 1;
-            dp[i][j] = fmin(
-                fmin(dp[i - 1][j] + 1,    // Deletion
-                     dp[i][j - 1] + 1),   // Insertion
-                dp[i - 1][j - 1] + cost); // Substitution
+        for (int j = 0; j <= n; j++) {
+            if (i == 0) {
+                dp[i][j] = j; // Initialize the first row
+            } else if (j == 0) {
+                dp[i][j] = i; // Initialize the first column
+            } else {
+                int cost = (word1[i - 1] == word2[j - 1]) ? 0 : 1;
+                dp[i][j] = fmin(
+                    fmin(dp[i - 1][j] + 1,    // Deletion
+                         dp[i][j - 1] + 1),   // Insertion
+                    dp[i - 1][j - 1] + cost); // Substitution
+            }
         }
     }
     
-    int result = dp[m][n];
-    return result;
+    return dp[m][n];
 }
