813d99dd6d4e09b7d780b5ae204262fbe90df28f
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-09-06 12:29:07 +0530

    Time: 298 ms (25%), Space: 67.2 MB (30.63%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.c
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.c
@@ -1,29 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 
 #define MAX_VAL 100000
 
 struct ListNode* modifiedList(int* nums, int n, struct ListNode* head) {
-    if (head == NULL) {
-        return head;
+    unsigned char hash[MAX_VAL / 8 + 1] = {0};
+    int i;
+    
+    for (i = 0; i < n; i++) {
+        hash[nums[i] / 8] |= (1 << (nums[i] % 8));
     }
-
-    // Create a hash set
-    bool hash_set[MAX_VAL + 1] = {false};
-    for (int i = 0; i < n; i++) {
-        hash_set[nums[i]] = true;
-    }
-
+    
     struct ListNode dummy = {0, head};
-    struct ListNode* current = &dummy;
-
-    while (current->next != NULL) {
-        if (hash_set[current->next->val]) {
-            struct ListNode* temp = current->next;
-            current->next = current->next->next;
+    struct ListNode* curr = &dummy;
+    
+    while (curr->next) {
+        if (hash[curr->next->val / 8] & (1 << (curr->next->val % 8))) {
+            struct ListNode* temp = curr->next;
+            curr->next = temp->next;
             free(temp);
         } else {
-            current = current->next;
+            curr = curr->next;
         }
     }
-
+    
     return dummy.next;
 }
