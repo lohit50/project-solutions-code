ca5a7216a58f99d62da2e71ae6ecc54481a84e7d
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-09-07 20:15:57 +0530

    Time: 1 ms (95.21%), Space: 45 MB (5.71%) - LeetHub

--- 3324-split-the-array/3324-split-the-array.java
+++ 3324-split-the-array/3324-split-the-array.java
@@ -1,12 +1,22 @@
 class Solution {
     public boolean isPossibleToSplit(int[] nums) {
-        HashMap <Integer,Integer> map = new HashMap<>();
 
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0) +1);
-        }
-        for(int i =0;i<nums.length;i++){
-            if(map.get(nums[i]) > 2){
+        // // hastechnique : 
+        // HashMap <Integer,Integer> map = new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0) +1);
+        // }
+        // for(int i =0;i<nums.length;i++){
+        //     if(map.get(nums[i]) > 2){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        int[] count = new int[1000];
+        for(int num:nums){
+            count[num]++;
+            if(count[num] > 2){
                 return false;
             }
         }
