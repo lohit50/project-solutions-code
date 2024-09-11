887ab1fa8dc8f4d00ef9010170399ce632f5560a
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-09-11 11:17:35 +0530

    Time: 9 ms (6.95%), Space: 40.7 MB (24.67%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -1,22 +1,20 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        // Convert integers to binary strings
+
         String start_bin = Integer.toBinaryString(start);
         String end_bin = Integer.toBinaryString(goal);
 
-        // Print binary representations for debugging
         System.out.printf("%s\n", start_bin);
         System.out.printf("%s\n", end_bin);
 
-        // Determine the maximum and minimum length of the binary strings
         int maxLength = Math.max(start_bin.length(), end_bin.length());
         int minLength = Math.min(start_bin.length(), end_bin.length());
 
         int count = 0;
         int remain = 0;
 
-        for (int i = 0, j = 0; i < minLength && j < minLength; i++, j++) {
-            if (start_bin.charAt(start_bin.length() - minLength + i) != end_bin.charAt(end_bin.length() - minLength + j)) {
+        for (int i = start_bin.length() - 1, j = end_bin.length() - 1; i >= 0 && j >= 0; i--, j--) {
+            if (start_bin.charAt(i) != end_bin.charAt(j)) {
                 count++;
             }
         }
