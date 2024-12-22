dddce86b14c15dec0e8daaf5eeb38d6d6984bc42
L O H I T <123096331+lohit50@users.noreply.github.com>
2024-12-22 21:26:48 +0530

    Time: 13 ms (37.75%), Space: 44.5 MB (85.12%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -3,23 +3,21 @@ class Solution {
         int number = 0;
         StringBuilder ran = new StringBuilder(ranstr);
         StringBuilder mag = new StringBuilder(magstr);
-        
-        for (int i = 0; i < ran.length(); i++) {
-            boolean found = false;
-            for (int j = 0; j < mag.length(); j++) {
-                if (ran.charAt(i) == mag.charAt(j)) {
-                    ran.setCharAt(i, '*'); // Mark ran's char as used
-                    mag.setCharAt(j, '*'); // Mark mag's char as used
-                    number++;
-                    found = true;
-                    break; 
+        for(int i=0;i<ran.length();i++){
+            boolean found= false;
+                for(int j=0;j<mag.length();j++){
+                    if(ran.charAt(i) == mag.charAt(j)){
+                        ran.setCharAt(i,'*');
+                        mag.setCharAt(j,'*');
+                        number++;
+                        found = true;
+                        break;
+                    }
                 }
-            }
-            if (!found) {
-                return false; 
-            }
+                if(!found){
+                    return false;
+                }
         }
-        
-        return number == ran.length();
+        return  number == ran.length();
     }
 }
