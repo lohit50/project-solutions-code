144207d4c8325b6eab5b58f50fa4a3981bca63d6
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-01-23 21:08:21 +0530

    Time: 7 ms (20.82%), Space: 52.4 MB (90.16%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,30 +1,25 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
         if (trust.length == 0 && n == 1) return n;
-        int[] people  = new int[n + 1];
+
+        int[] trusted  = new int[n + 1];
         int[] trusters = new int[n + 1];
+
         int res = -1;
-        for (int[] t: trust) {
+        
+        for(int t[] : trust){
             trusters[t[0]] += 1;
-            int yaru = t[0];
-            if (res == yaru) return -1;
-            int yara = t[1];
-            people[yara] += 1;
-            if (people[yara] == n - 1) res = yara;
+            trusted[t[1]] += 1;
         }
-        System.out.println(Arrays.toString(people));
-        System.out.println(Arrays.toString(trusters));
-        int max = 0;
-        for(int i=0; i<people.length;i++){
-            if(people[i] > max){
-                max = people[i];
-                res = i;
-            }else if(people[i] == max){
-                res = -1;
+        for(int i=1; i<=n; i++){
+            if(trusters[i] == 0 && trusted[i] == n-1){
+                res = i; 
             }
         }
-        if(res != -1 && trusters[res] != 0) res = -1;
-        if(max != n-1) res = -1;
+
+        System.out.println(Arrays.toString(trusted));
+        System.out.println(Arrays.toString(trusters));
+
         return res;
     }
 }
