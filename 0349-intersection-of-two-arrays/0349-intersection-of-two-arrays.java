b4f7fcb326d0b7d58e010c87c4a990edef9ad7d6
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-01-24 12:24:51 +0530

    Time: 0 ms (100%), Space: 43.2 MB (81.46%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,31 +1,19 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        int[] combined = new int[nums1.length + nums2.length + 1];
-        System.arraycopy(nums1, 0, combined, 0, nums1.length);
-        System.arraycopy(nums2, 0, combined, nums1.length+1, nums2.length);
-        combined[nums1.length] = 6969;
-        System.out.println(Arrays.toString(combined));
-
-        int[] result = new int[combined.length];
+        int[] result = new int[nums1.length + nums2.length];
         int index = 0;
+        int[] visited = new int[1001];
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int flag = 0;
-        for(int i : combined){
-            if(i == 6969) flag = 1;
-            else if(flag == 0) map.put(i, map.getOrDefault(i, 0) + 1);
-            else if(flag == 1){
-                if(map.containsKey(i)){
-                    if(map.get(i) < 0) continue;
-                    else{
-                        map.put(i, map.getOrDefault(i, 0) - 1000000);
-                        result[index++] = i;
-                    }
-                }
+        for(int i : nums1){
+            visited[i] = 1;
+        }
+
+        for(int i : nums2){
+            if(visited[i] == 1 ){
+                result[index++] = i;
+                visited[i] = 0;
             }
         }
-        System.out.printf("answer finding array \n");
-    
         return Arrays.copyOf(result,index);
     }
 }
