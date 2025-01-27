bdf8b6252e4e857581f218b39fea36b1fa184cd6
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-01-27 14:18:01 +0530

    Time: 4 ms (50.91%), Space: 45 MB (61.09%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,33 +1,48 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
 
-    public Boolean vowel(Character letter){
-        if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ||
-           letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U' ){
-                return true;
-           }
-        return false;
+    // Predefined Set of vowels for fast lookup
+    private static final Set<Character> VOWELS = new HashSet<>();
+
+    static {
+        for (char c : "aeiouAEIOU".toCharArray()) {
+            VOWELS.add(c);
+        }
+    }
+
+    // Function to check if a character is a vowel
+    public boolean isVowel(char letter) {
+        return VOWELS.contains(letter);
     }
 
     public String reverseVowels(String s) {
         StringBuilder string = new StringBuilder(s);
-        
+
         int left = 0;
         int right = string.length() - 1;
 
-        while(left < right){
-            if(vowel(string.charAt(left)) && vowel(string.charAt(right))){
+        while (left < right) {
+            // Store results of isVowel to avoid redundant checks
+            boolean leftIsVowel = isVowel(string.charAt(left));
+            boolean rightIsVowel = isVowel(string.charAt(right));
+
+            if (leftIsVowel && rightIsVowel) {
+                // Swap vowels
                 char temp = string.charAt(left);
                 string.setCharAt(left, string.charAt(right));
                 string.setCharAt(right, temp);
-                System.out.print("lohit");
+
                 left++;
                 right--;
-            }else if(! vowel(string.charAt(left))){
+            } else if (!leftIsVowel) {
                 left++;
-            }else{
+            } else {
                 right--;
             }
         }
+
         return string.toString();
     }
 }
