64fa8e7e5fd02c1e8f662f25fd22581f97e3531f
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-01-28 14:48:31 +0530

    Time: 3 ms (7.68%), Space: 44.3 MB (91.26%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static void Generate(int[] nums, List<Integer> sol ,List<List<Integer>> ans){
+        if(sol.size()  == nums.length ){
+            ans.add(new ArrayList<>(sol));                   ///// BASE CASES
+            return;
+        }
+        for(int i : nums){
+            if(!sol.contains(i)){
+                sol.add(i);
+                Generate(nums,sol,ans);
+                sol.remove(sol.size() - 1);
+            }
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> sol = new ArrayList<>(); 
+        Generate(nums, sol, ans);
+        return ans;
+    }
+}
