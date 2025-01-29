cf7b741c4c4c96fa13ee2a27873c8c3c6300857c
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-01-29 11:09:32 +0530

    Time: 1 ms (99.87%), Space: 44.6 MB (80.24%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,17 +1,27 @@
 class Solution {
-    private void backtrack(int start,List<List<Integer>> res,List<Integer> combination,int[] candidates,int target){
-        if(target == 0) {res.add(new ArrayList<>(combination)); return;}
-        if(target<0) return;
-        for(int i=start;i<candidates.length;i++){
-            combination.add(candidates[i]);
-            backtrack(i,res,combination,candidates,target-candidates[i]);
-            combination.removeLast();
+
+
+    public static void Generate(int[] candidates, int target, int remain, List<Integer> sol, List<List<Integer>> ans, int number){
+        if(remain == 0){
+            ans.add(new ArrayList(sol));        /// Base case
+            return;
+        }
+        for(int i = number ; i < candidates.length; i++){
+            if(remain - candidates[i] >= 0){
+                sol.add(candidates[i]);
+                Generate(candidates, target, remain - candidates[i], sol, ans, i);
+                sol.remove(sol.size()-1);
+            }
         }
     }
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> combination = new ArrayList<>();
-        backtrack(0,res,combination,candidates,target);
-        return res;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> sol = new ArrayList<>();
+        int remain = target;
+        int number = 0;
+        Generate(candidates, target, remain, sol, ans, number);
+        return ans;
+
     }
 }
