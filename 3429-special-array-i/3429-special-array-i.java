3ee052d012eff740c76c48697ede94ea818e7e2b
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-02-03 16:21:09 +0530

    Time: 1 ms (33.49%), Space: 43.4 MB (49.58%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -3,8 +3,7 @@ class Solution {
         if(nums.length == 1) return true;
         
        for(int i = 0; i < nums.length - 1; i++){
-        if(nums[i] % 2 == 0 && nums[i +1] % 2 == 0) return false;
-        if(nums[i] % 2 != 0 && nums[i +1] % 2 != 0) return false;
+            if( ( nums[i] % 2)  ==  ( nums[i + 1] % 2)) return false;
        }
         return true;
     }
