f20807bbfb1a678353f2ce686cf5b89517b60ae4
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-02-05 23:04:14 +0530

    Time: 0 ms (100%), Space: 41.8 MB (11.98%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
@@ -6,10 +6,8 @@ class Solution {
         for(int i = 1 ; i < nums.length; i++){
             if(nums[i - 1] < nums[i]){
                 max += nums[i];
-                System.out.println("added " + nums[i] + "to " + max);
             }else{
                 result = Math.max(result,max);
-                System.out.println(result);
                 max = nums[i];
             }
             result = Math.max(result,max);
