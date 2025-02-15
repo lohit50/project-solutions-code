1e51c34fd914e233872fbd0ea2e4fef847958b55
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-02-15 10:43:29 +0530

    Time: 41 ms (53.85%), Space: 55.8 MB (68.69%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,27 +1,26 @@
 class Solution {
 
-    public int DigitSum(int num){
-        int out = 0;
-        while(num > 0){
-            out += num%10;
+    public int DigitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
             num /= 10;
         }
-        return out;
+        return sum;
     }
 
     public int maximumSum(int[] nums) {
 
         Map<Integer, Integer> map = new HashMap<>();
-        int flag = 0;
         int result = -1;
 
-        for(int num : nums){
+        for (int num : nums) {
             int digitsum = DigitSum(num);
-            if(map.containsKey(digitsum)){
+            if (map.containsKey(digitsum)) {
                 result = Math.max(result, map.get(digitsum) + num);
             }
-            if(map.getOrDefault(digitsum,0) < num){
-                map.put(digitsum,num);
+            if (map.getOrDefault(digitsum, 0) < num) {
+                map.put(digitsum, num);
             }
         }
 
