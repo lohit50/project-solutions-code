9c2ce43915b2198474bb291cc89571f2cdd65cd5
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-02-16 18:21:26 +0530

    Time: 0 ms (100%), Space: 44.5 MB (49.55%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.java
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.java
@@ -8,12 +8,8 @@ class Solution {
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-
-            if (letters[mid] > target) {
-                end = mid - 1; 
-            } else {
-                start = mid + 1; 
-            }
+            if (letters[mid] > target) end = mid - 1;
+            else start = mid + 1;
         }
         
         return letters[start]; 
