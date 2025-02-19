cef0f453368fee78b6100d592ebd92eeea3221ef
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-02-19 18:06:19 +0530

    Time: 1 ms (99.09%), Space: 41.8 MB (74.72%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,36 +1,24 @@
+
 class Solution {
-    public static void Generate(int n,int[] count, StringBuilder result, List<String> list){
-        if(result.length() == n){
-            list.add(result.toString());
-             count[0]--; // Decrement count
-            if (count[0] == 0) return; // Stop when we reach the k-th string
-            System.out.println(result.toString());
-            return;
-        }
-        for(char ch : new char[]{'a','b','c'}){
-            if(result.charAt(result.length() - 1) != ch){
-                result.append(ch);
-                Generate(n,count,result,list);
-                 if (count[0] == 0) return; // Stop recursion when k-th string is found
-                result.deleteCharAt(result.length() -1);
-            }
-        }
-    }
-    
     public String getHappyString(int n, int k) {
-        int each_size = (int)Math.pow(2,(n-1));
-        int k_location = (k-1)/each_size;
-
-        if(k_location > 2) return "";
-
-        StringBuilder result = new StringBuilder();
-        List<String> list = new ArrayList<>();
-
-        result.append((char)(k_location + 97));
-        int[] count = {k - (k_location * each_size)};
-        Generate(n,count,result,list);
-
-        return result.toString();
+        int total = 1 << (n - 1); 
+        if (k > 3 * total) return ""; 
+        
+        StringBuilder sb = new StringBuilder();
+        char prev = (char) ('a' + (k - 1) / total); 
+        sb.append(prev);
+        
+        k = (k - 1) % total; 
+        
+        for (int i = 1; i < n; i++) {
+            total /= 2; 
+            char first = prev == 'a' ? 'b' : 'a'; 
+            char second = (char) ('a' + 'b' + 'c' - first - prev);
+            prev = (k / total == 0) ? first : second;
+            sb.append(prev);
+            k %= total;
+        }
+        
+        return sb.toString();
     }
-
 }
