167c17e482a30513385f365c5c141a1d79f713db
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-03 09:34:09 +0530

    Time: 1752 ms (5.36%), Space: 59.4 MB (98.19%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,32 +1,32 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int[] result = new int[nums.length];
-        int index = 0;
-
-        // int pass = 0;
-        // while(pass < 3){
-        //     for(int i = 0; i < nums.length ; i++){
-        //         if(pass == 0 && nums[i] < pivot) result[index++] = nums[i];
-        //         else if(pass == 1 && nums[i] == pivot) result[index++] = nums[i];
-        //         else if(pass == 2 && nums[i] > pivot) result[index++] = nums[i];
-        //     }
-        //     pass++;
-        // }
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] < pivot){
-                result[index++] = nums[i];
+        int n = nums.length;
+        int pos = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < pivot) {
+                for (int j = i; j > pos; j--) {
+                    swap(nums, j, j - 1);
+                }
+                pos++;
             }
         }
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == pivot){
-                result[index++] = nums[i];
+        
+        int pivotStart = pos;
+        for (int i = pos; i < n; i++) {
+            if (nums[i] == pivot) {
+                for (int j = i; j > pivotStart; j--) {
+                    swap(nums, j, j - 1);
+                }
+                pivotStart++;
             }
         }
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] > pivot){
-                result[index++] = nums[i];
-            }
-        }
-        return result;
+        
+        return nums;
+    }
+    
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
