50e9f3d2a4ac97745415a67d4c3c3aa728df9d22
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-03 18:38:23 +0530

    Time: 2 ms (70.81%), Space: 41.5 MB (64.78%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,16 +1,18 @@
+
 class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
-        int result = 0;
-        for (int i = 0; i < citations.length; i++) {
-            int remainingPapers = citations.length - i;
+        int n = citations.length;
+        int hIndex = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int remainingPapers = n - i;
             if (citations[i] >= remainingPapers) {
-                result = remainingPapers;
+                hIndex = remainingPapers;
+            } else {
                 break;
             }
         }
-        return result;
+        return hIndex;
     }
 }
-
-
