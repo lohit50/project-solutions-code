4b4396125574df9175f52b39748c565955811340
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-03 09:17:28 +0530

    Time: 5 ms (85.39%), Space: 66.8 MB (60.5%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -2,14 +2,30 @@ class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int[] result = new int[nums.length];
         int index = 0;
-        int pass = 0;
-        while(pass < 3){
-            for(int i = 0; i < nums.length ; i++){
-                if(pass == 0 && nums[i] < pivot) result[index++] = nums[i];
-                else if(pass == 1 && nums[i] == pivot) result[index++] = nums[i];
-                else if(pass == 2 && nums[i] > pivot) result[index++] = nums[i];
+
+        // int pass = 0;
+        // while(pass < 3){
+        //     for(int i = 0; i < nums.length ; i++){
+        //         if(pass == 0 && nums[i] < pivot) result[index++] = nums[i];
+        //         else if(pass == 1 && nums[i] == pivot) result[index++] = nums[i];
+        //         else if(pass == 2 && nums[i] > pivot) result[index++] = nums[i];
+        //     }
+        //     pass++;
+        // }
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] < pivot){
+                result[index++] = nums[i];
             }
-            pass++;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == pivot){
+                result[index++] = nums[i];
+            }
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > pivot){
+                result[index++] = nums[i];
+            }
         }
         return result;
     }
