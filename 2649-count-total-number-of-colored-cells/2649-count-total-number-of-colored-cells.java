c6dfe74ca6d2a2617095310535cb5534705eb212
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-05 09:09:32 +0530

    Time: 3 ms (42.21%), Space: 40.9 MB (9.84%) - LeetHub

---
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long coloredCells(int n) {
+        long result = 1;
+        int i = 0;
+        while(n > 0){
+            result += (4*i);
+            i++;
+            n--;
+        }
+        return result;
+    }
+}
