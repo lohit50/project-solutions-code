6348978e1b48c94d186860751f8e4ba91e4ee819
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-06 14:40:37 +0530

    Time: 1 ms (92.8%), Space: 44.8 MB (31.36%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,21 +1,21 @@
 class Solution {
-    public static void Generate(int[] nums, List<Integer> sol ,List<List<Integer>> ans){
-        if(sol.size() == nums.length){
-            ans.add(new ArrayList(sol));
+    public void Generate(int[] nums, List<List<Integer>> permutations, List<Integer> temp){
+        if(temp.size() == nums.length){
+            permutations.add(List.copyOf(temp));
+            return;
         }
-        for(int i : nums){
-            if(!sol.contains(i)){
-                sol.add(i);
-                Generate(nums,sol,ans);
-                sol.remove(sol.size()-1);
+        for(int i = 0; i < nums.length; i++){
+            if(!temp.contains(nums[i])){
+                temp.add(nums[i]);
+                Generate(nums,permutations,temp);
+                temp.remove(temp.size() - 1);
             }
         }
     }
-
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> sol = new ArrayList<>(); 
-        Generate(nums, sol, ans);
-        return ans;
+        List<List<Integer>> permutations = new ArrayList();
+        List<Integer> temp = new ArrayList();
+        Generate(nums,permutations,temp);
+        return permutations;
     }
 }
