4f489185fb6a32a9723e4212319f48c62a104a53
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-07 12:24:57 +0530

    Time: 360 ms (16%), Space: 41 MB (98%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -2,8 +2,7 @@ class Solution {
     public boolean isPrime(int n){
         if(n <= 1) return false;
         if(n <= 3) return true;
-        if(n == 5 || n == 7 || n == 11 || n == 13 || n == 17 || n == 19) return true;
-        if(n%2==0 || n%3==0 || n%5==0 || n%7 == 0 || n%11 == 0 || n%13 == 0 || n%17 == 0 ||n%19 == 0) return false;
+        if(n%2==0 || n%3==0) return false;
         else{
             for (int i = 5; i*i <= n; i += 6) {
                 if (n % i == 0 || n % (i + 2) == 0) return false;
@@ -18,6 +17,7 @@ class Solution {
         pair[1] = 3;
         if(left == 1 && right > 2) return pair;
         for(int i = left; i <= right; i++){
+            if(i > 2 && i%2 == 0) continue;
             if(isPrime(i)){
                 if(pair[0] == 2) pair[0] = i;
                 else if(pair[1] == 3) pair[1] = i;
