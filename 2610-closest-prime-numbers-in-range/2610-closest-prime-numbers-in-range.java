ff3efb536ba7473027f3a5d30198daaac17186c8
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-07 13:39:16 +0530

    Time: 359 ms (16%), Space: 55 MB (18.8%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -1,46 +1,44 @@
 class Solution {
-    public static boolean[] primeList; // Stores precomputed prime status
-
-    public void precomputePrimes(int limit) {
-        primeList = new boolean[limit + 1];
-        Arrays.fill(primeList, true);
-        primeList[0] = primeList[1] = false; // 0 and 1 are not prime
-
-        // Sieve of Eratosthenes
-        for (int i = 2; i * i <= limit; i++) {
-            if (primeList[i]) {
-                for (int j = i * i; j <= limit; j += i) {
-                    primeList[j] = false;
+    public static int[] primeList;
+    public boolean isPrime(int n){
+        if(n <= 1) return false;
+        if(n <= 3) return true;
+        if(n%2==0 || n%3==0) return false;
+        else{
+            for (int i = 5; i*i <= n; i += 6) {
+                if (n % i == 0 || n % (i + 2) == 0){
+                    return false;
                 }
             }
         }
+        return true;
     }
-
-    public boolean isPrime(int n) {
-        if (n < primeList.length) {
-            return primeList[n]; // Fast lookup from precomputed list
-        }
-        return false; // If out of range, consider non-prime (can be expanded dynamically)
-    }
-
     public int[] closestPrimes(int left, int right) {
-        precomputePrimes(right); // Compute primes up to `right`
-        int[] pair = new int[]{-1, -1};
-        int last_prime = -1;
-
-        for (int i = left; i <= right; i++) {
-            if (i > 2 && i % 2 == 0) continue; // Skip even numbers
-            if (isPrime(i)) {
-                if (last_prime != -1) {
-                    if (pair[0] == -1 || (i - last_prime < pair[1] - pair[0])) {
+        primeList = new int[right - left];
+        int[] pair = new int[2];
+        int last_prime = 2;
+        pair[0] = 2;
+        pair[1] = 3;
+        if(left == 1 && right > 2) return pair;
+        for(int i = left; i <= right; i++){
+            if(i > 2 && i%2 == 0) continue;
+            if(isPrime(i)){
+                if(pair[0] == 2) pair[0] = i;
+                else if(pair[1] == 3) pair[1] = i;
+                else{
+                    if(pair[1] - pair[0] > i - last_prime){
                         pair[0] = last_prime;
                         pair[1] = i;
                     }
                 }
                 last_prime = i;
+                i++;
             }
         }
-
+        if( pair[0] == 2 || pair[1] == 3 ){
+            pair[0] = -1;
+            pair[1] = -1;
+        }
         return pair;
     }
 }
