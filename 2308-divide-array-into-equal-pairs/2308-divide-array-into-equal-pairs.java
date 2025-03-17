a3cbd2d5b7f5d72621e0ed3dbe6cd7608c06389e
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-17 09:32:31 +0530

    Time: 2 ms (82.79%), Space: 44 MB (97.54%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean divideArray(int[] nums) {
         int[] frequency = new int[501];
-        
-        for (int num : nums) {
-            frequency[num]++;
+        int xor = nums[0];
+        frequency[nums[0]]++;
+        for(int i = 1; i < nums.length; i++){
+            frequency[nums[i]]++;
+            xor = xor ^ nums[i];
         }
-        
-        for (int count : frequency) {
-            if ((count & 1) != 0) return false;
-        }
+        if(xor != 0) return false;
+        for(int i = 0; i < frequency.length; i++) if(frequency[i] % 2 != 0) return false;
         return true;
     }
 }
