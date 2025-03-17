506655d5c61bc4c7fbca9e9cdac88319232e186e
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-17 09:25:13 +0530

    Time: 33 ms (27.33%), Space: 62.6 MB (93.45%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {
-        int l = 0, r = (int)1e7;
-        while (l < r) {
-            int m = (l + r + 1) / 2;
-            long s = 0;
-            for (int p : candies) s += p / m;
-            if (s < k) r = m - 1;
-            else l = m;
-        }
+       int l = 0 , r = (int)1e7;
+       while(l < r){
+        int mid = (r + l + 1) / 2;
+        long s = 0;
+        for(int i : candies) s += i/mid;
+        if(s < k) r = mid - 1;
+        else l = mid;
+       }
         return l;
     }
 }
