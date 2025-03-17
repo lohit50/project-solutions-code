170edc416ed4b60a4f740c872e3e04cc1f860192
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-17 09:56:49 +0530

    Time: 6 ms (45.22%), Space: 44.8 MB (29.76%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -1,17 +1,11 @@
 class Solution {
     public boolean divideArray(int[] nums) {
-//         int[] frequency = new int[501];
-//         int xor = nums[0];
-//         frequency[nums[0]]++;
-//         for(int i = 1; i < nums.length; i++){
-//             frequency[nums[i]]++;
-//             xor = xor ^ nums[i];
-//         }
-//         if(xor != 0) return false;
-//         for(int i = 0; i < frequency.length; i++) if(frequency[i] % 2 != 0) return false;
-        int[] frequency = new int[501];
-        for (int num : nums) frequency[num]++;
-        for (int count : frequency) if (count % 2 != 0) return false;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums){
+            if(set.contains(i)) set.remove(i);
+            else set.add(i);
+        }
+        if(set.size() != 0) return false;
         return true;
     }
 }
