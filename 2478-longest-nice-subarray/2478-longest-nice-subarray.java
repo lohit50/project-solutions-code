d6d1d62b994888e1a684d63742a9bb4fbe5e91ac
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-18 19:05:17 +0530

    Time: 5 ms (17.98%), Space: 55.6 MB (67.85%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.java
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -3,15 +3,15 @@ class Solution {
         int longest = 1;
         int l = 0;
         int r = 0;
-        while (r < nums.length - 1) {
-            r++;
-            if ((nums[r] & nums[r - 1]) == 0) {
-                while (!checkAll(nums, l, r)) {
-                    l++;
-                }
+
+        while (r < nums.length) {
+            if (checkAll(nums, l, r)) { 
                 longest = Math.max(longest, r - l + 1);
-            } 
-            else l = r;
+                r++;
+            } else {
+                l++;
+                if (l > r) r = l;
+            }
         }
         return longest;
     }
