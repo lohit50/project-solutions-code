63ebc4119923686d2ba112360745fc0a5a8aa56d
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-19 00:56:28 +0530

    Time: 54 ms (5.21%), Space: 45.7 MB (5.55%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<String,Integer> map = new HashMap();
+        map.put("I",1);
+        map.put("V",5);
+        map.put("IV",4);
+        map.put("X",10);
+        map.put("IX",9);
+        map.put("XL",40);
+        map.put("L",50);
+        map.put("XC",90);
+        map.put("C",100);
+        map.put("CD",400);
+        map.put("D",500);
+        map.put("CM",900);
+        map.put("M",1000);
+        int result = 0;
+        for(int i = 0; i < s.length(); i++){
+            String pull;
+            if(s.charAt(i) == 'X'){
+                if(i < s.length() - 1 && s.charAt(i+1) == 'L') pull = "XL";
+                else if(i < s.length() - 1 && s.charAt(i+1) == 'C') pull = "XC";
+                else pull = "X";
+            }
+            else if(s.charAt(i) == 'C'){
+                if(i < s.length() - 1 && s.charAt(i+1) == 'M') pull = "CM";
+                else if(i < s.length() - 1 && s.charAt(i+1) == 'D') pull = "CD";
+                else pull = "C";
+            }
+            else if(s.charAt(i) == 'I'){
+                if(i < s.length() - 1 && s.charAt(i+1) == 'V') pull = "IV";
+                else if(i < s.length() - 1 && s.charAt(i+1) == 'X') pull = "IX";
+                else pull = "I";
+            }
+            else {
+                pull = Character.toString(s.charAt(i));
+            }
+            result += map.get(pull);
+            if(pull.length() == 2) i++;
+            System.out.println(result);
+        }
+        return result;
+    }
+}
