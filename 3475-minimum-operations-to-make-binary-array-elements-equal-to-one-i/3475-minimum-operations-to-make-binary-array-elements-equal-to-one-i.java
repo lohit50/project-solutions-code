ae7b33dec507c5c00200bf8a46921203d22329ce
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-19 10:31:04 +0530

    Time: 7 ms (66.53%), Space: 57.4 MB (61.4%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -2,32 +2,37 @@ class Solution {
     public int minOperations(int[] nums) {
         int minimum = 0;
         int Next_zero = 0;
+        int One_count = 0;
         int i = 0;
         while( i < nums.length - 2){
             if(nums[i] == 1){
                 i++;
+                One_count++;
                 continue;
             }
             else{
                 minimum++;
-
-                // i change
+                // i change raa
                 nums[i] = 1;
+                One_count++;
 
-                // i+1 change
+                // i+1 change raa
                 if(nums[i] == 1) Next_zero = i+1;
+                else One_count++;
                 nums[i+1] ^= 1;
 
-                // i+2 change
+                // i+2 change raa
                 if(nums[i+2] == 1 && Next_zero != i+1) Next_zero = i+2;
+                if(nums[i+2] == 0 && ((Next_zero != i+1) && (Next_zero != i+2))) One_count++;
                 nums[i+2] ^= 1;
 
             i = Next_zero;
             }
         }
-        for(i = 0; i < nums.length; i++){
-            if(nums[i] == 0) return -1;
+        if(nums[nums.length - 2] == 1 && nums[nums.length - 1] == 1){
+           One_count += 2;
         }
+        if(One_count != nums.length) return -1;
         if(minimum == 0) return 0;
         return minimum;
     }
