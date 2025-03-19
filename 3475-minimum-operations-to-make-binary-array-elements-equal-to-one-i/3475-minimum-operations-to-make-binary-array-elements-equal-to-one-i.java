d77e6edd8088f193da1f49db5d5c8ea60ea208fc
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-19 10:33:26 +0530

    Time: 6 ms (100%), Space: 57.6 MB (35.32%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,39 +1,20 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int minimum = 0;
-        int Next_zero = 0;
-        int One_count = 0;
-        int i = 0;
-        while( i < nums.length - 2){
-            if(nums[i] == 1){
-                i++;
-                One_count++;
-                continue;
-            }
-            else{
-                minimum++;
-                // i change raa
-                nums[i] = 1;
-                One_count++;
+        int n = nums.length;
+        int operations = 0;
 
-                // i+1 change raa
-                if(nums[i] == 1) Next_zero = i+1;
-                else One_count++;
-                nums[i+1] ^= 1;
-
-                // i+2 change raa
-                if(nums[i+2] == 1 && Next_zero != i+1) Next_zero = i+2;
-                if(nums[i+2] == 0 && ((Next_zero != i+1) && (Next_zero != i+2))) One_count++;
-                nums[i+2] ^= 1;
-
-            i = Next_zero;
+        for (int i = 0; i <= n - 3; ++i) {
+            if (nums[i] == 0) {
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
+                operations++;
             }
         }
-        if(nums[nums.length - 2] == 1 && nums[nums.length - 1] == 1){
-           One_count += 2;
+
+        if (nums[n - 1] == 0 || nums[n - 2] == 0) {
+            return -1;
         }
-        if(One_count != nums.length) return -1;
-        if(minimum == 0) return 0;
-        return minimum;
+
+        return operations;
     }
 }
