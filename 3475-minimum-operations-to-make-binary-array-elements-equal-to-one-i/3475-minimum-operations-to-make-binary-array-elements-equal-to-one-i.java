3594e171eefccf3c68ebd71bfc6730ee4d461773
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-19 10:13:54 +0530

    Time: 7 ms (66.53%), Space: 57.3 MB (74.95%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,12 +1,10 @@
 class Solution {
     public int minOperations(int[] nums) {
         int minimum = 0;
-        int One_count = 0;
         int Next_zero = 0;
         int i = 0;
         while( i < nums.length - 2){
             if(nums[i] == 1){
-                One_count++;
                 i++;
                 continue;
             }
@@ -15,30 +13,17 @@ class Solution {
 
                 // i change
                 nums[i] = 1;
-                One_count++;
 
                 // i+1 change
-                if(nums[i+1] == 1){
-                    nums[i+1] = 0;
-                    Next_zero = i+1;
-                }else{
-                    nums[i+1] = 1;
-                }
+                if(nums[i] == 1) Next_zero = i+1;
+                nums[i+1] ^= 1;
 
                 // i+2 change
-                if(nums[i+2] == 1){
-                    nums[i+2] = 0;
-                    if(Next_zero != i+1){
-                        Next_zero = i+2;
-                    }
-                } else{
-                    nums[i+2] = 1;
-                }
+                if(nums[i+2] == 1 && Next_zero != i+1) Next_zero = i+2;
+                nums[i+2] ^= 1;
+
             i = Next_zero;
             }
-            // System.out.println(Next_zero);
-            // System.out.println(One_count);
-            // System.out.println(Arrays.toString(nums));
         }
         for(i = 0; i < nums.length; i++){
             if(nums[i] == 0) return -1;
