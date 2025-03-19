7cdc87811ec19f8db8797ac392deb3d886fab4ce
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-19 11:56:13 +0530

    Time: 0 ms (100%), Space: 42 MB (95.39%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,9 +1,9 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        for(int i = 0; i < matrix.length; i++) System.out.println(Arrays.toString(matrix[i]));
-
-        System.out.println();
-        int last = matrix.length - 1;
+        transpose(matrix);
+        reverseRow(matrix);
+    }
+    public static void transpose(int[][] matrix){
         for(int i = 0; i < matrix.length; i++){
             for(int j = i+1; j < matrix.length; j++){
                 int temp = matrix[i][j];
@@ -11,14 +11,18 @@ class Solution {
                 matrix[j][i] = temp;
             }
         }
+    }
+    public static void reverseRow(int[][] matrix){
         for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix.length/2; j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix.length - 1 - j];
-                matrix[i][matrix.length - 1 - j] = temp;
+            int l = 0;
+            int r = matrix.length - 1;
+            while(l < r){
+                int temp = matrix[i][l];
+                matrix[i][l] = matrix[i][r];
+                matrix[i][r] = temp;
+                l++;
+                r--;
             }
         }
-        for(int i = 0; i < matrix.length; i++) System.out.println(Arrays.toString(matrix[i]));
-        return;
     }
 }
