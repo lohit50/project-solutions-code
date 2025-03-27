c3af5d47dda633887c9b1b9fe1f5ddf55f108fcb
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-03-27 14:59:09 +0530

    Time: 33 ms (54.49%), Space: 61.7 MB (23.08%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,27 +1,32 @@
+import java.util.*;
+
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        
-        Map<Integer,Integer> map = new HashMap<>();
+        int n = nums.size();
+        Map<Integer, Integer> map = new HashMap<>();
 
-        // fnd dom
-        int dominant = -1;
-        int freq = 0;
-        for(int i = 0 ; i < nums.size(); i++){
-            map.put(nums.get(i), map.getOrDefault(nums.get(i) , 0) + 1);
-            if(map.get(nums.get(i)) > freq){
-                freq = map.get(nums.get(i));
-                dominant = nums.get(i);
+        // Step 1: Find the dominant element
+        int dominant = -1, freq = 0;
+        for (int num : nums) {
+            int newCount = map.getOrDefault(num, 0) + 1;
+            map.put(num, newCount);
+            if (newCount > freq) {
+                freq = newCount;
+                dominant = num;
             }
         }
 
-        // fnd dom validity 
-        int count = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums.get(i) == dominant){
-                count++;
-                if(count * 2 > (i+1)){
-                    if( (freq - count) * 2 > nums.size() - (i+1) ) return i;
-                }
+        // Step 2: Find the minimum valid split index
+        int leftCount = 0; // Count of dominant element in left partition
+        for (int i = 0; i < n; i++) {
+            if (nums.get(i) == dominant) leftCount++;
+
+            int leftSize = i + 1;
+            int rightSize = n - leftSize;
+            int rightCount = freq - leftCount; // Remaining count in the right partition
+
+            if (leftCount * 2 > leftSize && rightCount * 2 > rightSize) {
+                return i;
             }
         }
         return -1;
