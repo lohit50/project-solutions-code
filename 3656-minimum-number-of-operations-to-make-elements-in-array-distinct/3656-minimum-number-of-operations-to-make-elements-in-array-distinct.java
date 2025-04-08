9abf299e2beab7364900a3d19448d80c9f7164fb
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-04-08 22:27:29 +0530

    Time: 1 ms (98.39%), Space: 44.9 MB (16.29%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -3,10 +3,7 @@ class Solution {
         int[] arr = new int[101];
         int i;
         for(i = nums.length - 1; i >= 0; i--){
-            arr[nums[i]]++;
-            if(arr[nums[i]] > 1 ){
-                return (i + 3) / 3;
-            }
+            if(++arr[nums[i]] > 1 ) return (i + 3) / 3;
         }
         return 0;
     }
