a74ab2b1227a0b24d8efb2feaf1013ea65420e90
L O H I T <123096331+lohit50@users.noreply.github.com>
2025-07-19 13:49:21 +0530

    Time: 2 ms (98.97%), Space: 42.4 MB (39.1%) - LeetHub

--- 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.java
@@ -3,14 +3,12 @@ class Solution {
         int result = 0;
         Arrays.sort(cost);
         if(cost.length == 1) return cost[0];
-        // System.out.println(Arrays.toString(cost));
         for(int i = cost.length - 1; i >= 0; i-=3){
             if(i == 0){
                 result += cost[i];
-                continue;
+                break;
             }
             result += cost[i]+cost[i-1];
-            // System.out.println(cost[i] + " " + cost[i-1]);
         }
         return result;
     }
